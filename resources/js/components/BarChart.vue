35a5811b5b128c10058726aab89e39f4390f4082
Kelly <kielbermudez200104@gmail.com>
2024-08-22 20:33:55 +0800

    bargraph function added

---
+++ resources/js/components/BarChart.vue
@@ -0,0 +1,95 @@
+<template>
+    <div>
+        <Bar :data="chartData" :options="chartOptions" />
+    </div>
+</template>
+
+<script setup>
+import { computed, onMounted, ref, watch } from 'vue';
+import { Bar } from 'vue-chartjs';
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js';
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale);
+
+
+const dataOfSumCategory = ref({})
+const sumOfCategory = () => {
+    axios({
+        method: 'GET',
+        url: '/api/bargraph'
+    }).then(response => {
+        dataOfSumCategory.value = response.data
+        // chartData.value.datasets[0].data[1] = 30
+        // console.log( chartData.value.datasets[0].data);
+        // console.log(dataOfSumCategory.value.materials);
+
+    })
+}
+
+
+const chartData = computed(() => ({
+    labels: [
+        "Tools",
+        "Materials"
+    ],
+    datasets: [{
+        label: 'consumed of recent used materials',
+        data: [dataOfSumCategory.value.tools,dataOfSumCategory.value.materials],
+        backgroundColor: ['skyblue'],
+    }, ],
+    fill: false,
+}));
+
+// const chartData = ref({
+//     labels: ['tools', 'materials'],
+//     datasets: [{
+//
+//         backgroundColor: ['green', 'yellow'],
+//         data: [20, 10],
+//         fill: false,
+//     }]
+// });
+
+
+const chartOptions = ref({
+    responsive: true,
+    plugins: {
+        legend: {
+            position: 'top'
+        },
+        tooltip: {
+            callbacks: {
+                label: function(tooltipItem) {
+                    return `${tooltipItem.dataset.label}: ${tooltipItem.raw}`;
+                }
+            }
+        }
+    },
+    scales: {
+        x: {
+            title: {
+                display: true,
+                text: 'test'
+            }
+        },
+        y: {
+            title: {
+                display: true,
+                text: 'test'
+            }
+        }
+    }
+});
+
+watch(chartData, (oldVal, NewVal) => {
+    console.log("dsds");
+})
+onMounted(() => {
+
+    sumOfCategory()
+
+})
+</script>
+
+<style scoped>
+/* Add any styles specific to your chart here */
+</style>
--- resources/js/pages/admin/AdminNewItem.vue
+++ resources/js/pages/admin/AdminNewItem.vue
@@ -13,82 +13,84 @@
                         <div class="row">
                             <div class="col">
                                 <label for="">Item Code <span class="text-danger" v-if="validation.item_code">
-                                    {{ validation.item_code[0] }}
-                                </span></label>
-                                <input type="text" class="form-control" placeholder="" v-model="input.item_code" >
+                                        {{ validation.item_code[0] }}
+                                    </span></label>
+                                <input type="text" class="form-control" placeholder="" v-model="input.item_code">
                             </div>
                         </div>
                         <div class="row">
                             <div class="col">
                                 <label for="">Brand: <span class="text-danger">
-                                    option
-                                </span></label>
-                                <input type="text" class="form-control" placeholder="" v-model="input.brand" >
+                                        option
+                                    </span></label>
+                                <input type="text" class="form-control" placeholder="" v-model="input.brand">
                             </div>
                         </div>
                         <div class="row">
                             <div class="col">
                                 <label for="">Supplier Name <span class="text-danger" v-if="validation.supplier_name">
-                                    {{ validation.supplier_name[0] }}
-                                </span></label>
+                                        {{ validation.supplier_name[0] }}
+                                    </span></label>
                                 <input type="text" class="form-control" placeholder="" v-model="input.supplier_name">
                             </div>
                             <div class="col">
                                 <label for="">Unit Cost: <span class="text-danger" v-if="validation.unit_cost" >
-                                    {{ validation.unit_cost[0] }}
-                                  </span></label>
+                                        {{ validation.unit_cost[0] }}
+                                      </span></label>
                                 <div class="input-group mb-3">
                                     <span class="input-group-text" id="basic-addon1">₱</span>
                                     <input type="number" class="form-control" placeholder="" v-model="input.unit_cost">
-                                  </div>
+                                </div>
                             </div>
                         </div>
                         <div class="row">
                             <div class="col">
                                 <label for="">Quantity: <span class="text-danger" v-if="validation.quantity">
-                                    {{ validation.quantity[0] }}
-                                </span></label>
+                                        {{ validation.quantity[0] }}
+                                    </span></label>
                                 <input type="number" class="form-control" placeholder="" v-model="input.quantity">
                             </div>
                             <div class="col">
                                 <label for="">Item Type: <span class="text-danger" v-if="validation.category">
-                                    {{ validation.category[0] }}
-                                </span></label>
+                                        {{ validation.category[0] }}
+                                    </span></label>
                                 <select class="form-select" v-model="input.category">
-                                    <option value="materials">materials</option>
-                                    <option value="tools">tools</option>
-                                </select>
+                                        <option value="materials">materials</option>
+                                        <option value="tools">tools</option>
+                                    </select>
                             </div>
                         </div>
-                       <div class="row">
-                        <div class="col">
-                            <label for="">Description: <span class="text-danger" v-if="validation.description">
-                                {{ validation.description[0] }}
-                            </span></label>
-                            <textarea name="" id="" cols="30" rows="10" class="form-control" v-model="input.description"></textarea>
+                        <div class="row">
+                            <div class="col">
+                                <label for="">Description: <span class="text-danger" v-if="validation.description">
+                                    {{ validation.description[0] }}
+                                </span></label>
+                                <textarea name="" id="" cols="30" rows="10" class="form-control" v-model="input.description"></textarea>
+                            </div>
                         </div>
-                       </div>
                         <div class="row">
                             <div class="col-1 generate-barcode">
                                 <label for="">Barcode:</label>
                                 <div>
                                     <BarcodeComponent :barcodeValue="barcodeValue" />
                                     <button class="btn btn-dark" @click="selectBarcode">Generate Barcode</button>
-                                  </div>
+                                </div>
                             </div>
                         </div>
                         <input type="hidden" v-model="barcodeValue">
                         <div class="row text-end">
-                            <div class="col">
+                            <div class="col action">
+                                <button class="btn btn-danger" @click="back">Back</button>
                                 <button class="btn btn-success" @click.enter="submit">Submit</button>
                             </div>
                         </div>
                     </div>
-                  </div>
-              </form>
+                </div>
+            </form>
         </div>
     </div>
 </template>
+
 <script setup>
 import Header from '@/components/Admin_Header.vue'
 import axios from 'axios';
@@ -114,24 +116,24 @@ const submit = () => {
         method: 'POST',
         url: 'api/new-item',
         data: {
-        item_code: input.value.item_code,
-        supplier_name: input.value.supplier_name,
-        unit_cost: input.value.unit_cost,
-        quantity: input.value.quantity,
-        category: input.value.category,
-        description: input.value.description,
-        brand: input.value.brand,
-        barcode: barcodeValue.value
+            item_code: input.value.item_code,
+            supplier_name: input.value.supplier_name,
+            unit_cost: input.value.unit_cost,
+            quantity: input.value.quantity,
+            category: input.value.category,
+            description: input.value.description,
+            brand: input.value.brand,
+            barcode: barcodeValue.value
         }
     }).then(response => {
         console.log(response);
-        if(response.status = 200){
+        if (response.status = 200) {
             router.push('/admin-inventory-list')
         }
     }).catch(res => {
         console.log(res);
-        if(res.response.status == 422){
-          validation.value = res.response.data.errors
+        if (res.response.status == 422) {
+            validation.value = res.response.data.errors
         }
     })
 }
@@ -142,33 +144,37 @@ const selectBarcode = () => {
         method: 'GET',
         url: 'api/generate-barcode',
     }).then(response => {
-      barcodeValue.value = response.data
+        barcodeValue.value = response.data
     })
 }
+const back = () => {
+    history.back()
+}
 onMounted(() => {
-  selectBarcode()
+    selectBarcode()
 })
-
-
-
 </script>
 
 <style scoped>
-
-form{
+form {
     width: 70rem;
     margin: auto;
-    display:grid;
-    gap:25px;
+    display: grid;
+    gap: 25px;
     border-radius: 10px;
-    padding:10px;
+    padding: 10px;
 }
-label{
+
+label {
     font-weight: 500;
 }
-.generate-barcode{
+
+.generate-barcode {
     display: grid;
     align-items: center;
-
+}
+.action{
+    display: flex;
+    justify-content: end;
 }
 </style>
