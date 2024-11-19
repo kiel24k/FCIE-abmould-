81f88bf9b9a134c0eb9bfd4f2fc347a33a4579dc
Kiel Alarcon Bermudez <kielbermudez200104@gmail.com>
2024-11-19 23:18:28 +0800

    admin revise txt added

--- mga kailangan i revise.js
+++ mga kailangan i revise.js
@@ -1,5 +1,15 @@
 const admin = [
-    
+    DASHBOARD [
+        {CALCULATION_ON_BAR_GRAPH: ""},
+        {CALCULATION_ON_LINE_GRAPH: ""},
+        {CALCULATION_ON_PIE_CHART: ""}
+    ],
+    SCHEDULE_REQUEST [
+        {PRINT_TABLE: ""},
+        {NUMBER_OF_PENDING_APPROVE_NOT_APPROVED_RELEASED: ""},
+        {}
+    ]
+
 ];
 
 const inventory_manager = [];
