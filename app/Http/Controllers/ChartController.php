8bf4039f51ce80c87d4f253816a226c398f3410b
Kiel Alarcon Bermudez <kielbermudez200104@gmail.com>
2024-11-21 23:41:30 +0800

    function for linegraph added

--- app/Http/Controllers/ChartController.php
+++ app/Http/Controllers/ChartController.php
@@ -10,11 +10,17 @@ class ChartController extends Controller
 {
     public function bargraph()
     {
-
         $label = DB::table('items')
             ->select('category', 'quantity')
             ->orderBy('id', 'DESC')
             ->get();
         return response()->json($label);
+    }
+    public function lineGraph () {
+        $data = DB::table('items')
+        ->select('item_code','unit_cost')
+        ->where('id' ,'>', 10)
+        ->get();
+        return response()->json($data);
     }
 }
