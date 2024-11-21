8abdd24360c6cb6ab6b2c547ba9cf3b619c492ad
Kiel Alarcon Bermudez <kielbermudez200104@gmail.com>
2024-11-21 22:55:43 +0800

    NEW_ITEM_CAN_ADD_ITEM_TYPE_DYNAMICALLY: DONE

--- mga kailangan i revise.js
+++ mga kailangan i revise.js
@@ -1,18 +1,37 @@
 const admin = [
-    DASHBOARD [
-        {CALCULATION_ON_BAR_GRAPH: ""},
-        {CALCULATION_ON_LINE_GRAPH: ""},
-        {CALCULATION_ON_PIE_CHART: ""}
+    DASHBOARD[
+        { CALCULATION_ON_BAR_GRAPH: "" },
+        { CALCULATION_ON_LINE_GRAPH: "" },
+        { CALCULATION_ON_PIE_CHART: "" }  
     ],
-    SCHEDULE_REQUEST [
-        {PRINT_TABLE: ""},
-        {NUMBER_OF_PENDING_APPROVE_NOT_APPROVED_RELEASED: ""},
-        {}
+    SCHEDULE_REQUEST[
+        { PRINT_TABLE: "" },
+        { NUMBER_OF_PENDING_APPROVE_NOT_APPROVED_RELEASED: "" }
+    ],
+    ITEM_LIST[
+        {NEW_ITEM_CAN_ADD_ITEM_TYPE_DYNAMICALLY: "DONE"}
     ]
-
+    
 ];
 
-const inventory_manager = [];
+const inventory_manager = [
+    HEADER[
+        { LOGOUT_STATEMENT: "" }
+    ],
+    DASHBOARD[
+        { FIXED_UI_FOR_GRAPH: "" },
+        { CALCULATION_ON_BAR_GRAPH: "" },
+        { CALCULATION_ON_LINE_GRAPH: "" },
+        { CALCULATION_ON_PIE_CHART: "" }
+    ],
+    SCHEDULE_REQUEST[
+        { PRINT_TABLE: "" },
+        { NUMBER_OF_PENDING_APPROVE_NOT_APPROVED_RELEASED: "" }
+    ],
+    ITEM_LIST[
+        {}
+    ]
+];
 
 const member = [];
 
