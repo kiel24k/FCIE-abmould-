ef8c9f12d40f63aa64e43adc06a757d9569420b5
Kiel Alarcon Bermudez <kielbermudez200104@gmail.com>
2024-12-07 10:59:37 +0800

    new tassk added

--- mga kailangan i revise.js
+++ mga kailangan i revise.js
@@ -1,18 +1,21 @@
 const admin = [
+    PROFILE[{ EDIT_PROFILE: "" }],
     DASHBOARD[
         ({ CALCULATION_ON_BAR_GRAPH: "DONE" },
         { CALCULATION_ON_LINE_GRAPH: "DONE" },
-        { CALCULATION_ON_PIE_CHART: "DONE" }
-    )
+        { CALCULATION_ON_PIE_CHART: "DONE" })
     ],
     SCHEDULE_REQUEST[
-        ({ PRINT_TABLE: "DONE" },
-        { NUMBER_OF_PENDING_APPROVE_NOT_APPROVED_RELEASED: "DONE" })
+        { PRINT_TABLE: "DONE" },
+        { NUMBER_OF_PENDING_APPROVE_NOT_APPROVED_RELEASED: "DONE" },
+        { PAGINATION_ON_TABLE: "" },
+        { NEXT_PAGE_PREV_PAGE_ON_TABLE: "" },
+        { SORTING_ON_TABLE: "" }
+        
     ],
     ITEM_LIST[
-        { NEW_ITEM_CAN_ADD_ITEM_TYPE_DYNAMICALLY: "DONE" },
-        {ITEM_LIST_WITH_MANY_CATEGORY: "DONE"}
-
+        ({ NEW_ITEM_CAN_ADD_ITEM_TYPE_DYNAMICALLY: "DONE" },
+        { ITEM_LIST_WITH_MANY_CATEGORY: "DONE" })
     ],
 ];
 
@@ -29,26 +32,27 @@ const inventory_manager = [
         { NUMBER_OF_PENDING_APPROVE_NOT_APPROVED_RELEASED: "DONE" })
     ],
     ITEM_LIST[
-        { FIX_UI_TABLE: "DONE" },
-        {ITEM_LIST_WITH_MANY_CATEGORY: "DONE"}
-        
+        ({ FIX_UI_TABLE: "DONE" }, { ITEM_LIST_WITH_MANY_CATEGORY: "DONE" })
     ],
 ];
 
 const member = [
-    ITEM_LIST [ 
-        {FIX_UI: "DONE"},
-        {FIX_LOGOUT_ON_SIDEBAR_AND_MOVE_TO_HEADER: "DONE"},
-        {LOGOUT_STATEMENT_ON_HEADER: "DONE"},
-        {ITEM_LIST_WITH_MANY_CATEGORY: "DONE"}
-    ]
+    ITEM_LIST[
+        ({ FIX_UI: "DONE" },
+        { FIX_LOGOUT_ON_SIDEBAR_AND_MOVE_TO_HEADER: "DONE" },
+        { LOGOUT_STATEMENT_ON_HEADER: "DONE" },
+        { ITEM_LIST_WITH_MANY_CATEGORY: "DONE" })
+    ],
 ];
 
 const tl = [
     Home[
-        { FIX_UI: "DONE" }
-        ],
+        { FIX_UI: "DONE" },
+        {CHANGE_CATEGORY_TO_TABLE_CATEGORY: ''}
+
+    ],
     SCHEDULE_REQUEST[
         { NUMBER_OF_PENDING_APPROVE_NOT_APPROVED_RELEASED: "DONE" }
     ],
+
 ];
