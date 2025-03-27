2bb3f6f1d29129100e52974fb95022996756c817
Kiel Alarcon Bermudez <kielbermudez200104@gmail.com>
2025-03-27 23:05:10 +0800

    migration for item logs added

---
+++ database/migrations/2025_03_27_230103_create_item_logs_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('item_logs', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->enum('action', ['created','update', 'deleted']);
+            $table->date('date_created');
+            $table->string('time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('item_logs');
+    }
+};
